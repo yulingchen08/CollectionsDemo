6d3a313cce53581bb464a71de2df28f2dfed0188
Yuling <yulingchen08@gmail.com>
2024-01-03 12:56:49 +0800

    git init

---
+++ CollectionsDemoUITests/CollectionsDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionsDemoUITestsLaunchTests.swift
+//  CollectionsDemoUITests
+//
+//  Created by 陳鈺翎 on 2024/1/3.
+//
+
+import XCTest
+
+final class CollectionsDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
