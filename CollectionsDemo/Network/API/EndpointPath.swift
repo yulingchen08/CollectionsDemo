9b0b83532cc873323a2d5a279ea38db3bb6b1919
Yuling <yulingchen08@gmail.com>
2024-01-09 01:02:51 +0800

    chore: Comment out

--- CollectionsDemo/Network/API/EndpointPath.swift
+++ CollectionsDemo/Network/API/EndpointPath.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 struct ApiConstants {
-    // TODO: Need to encrypt API key, but this is not the focus of the current assignment. Will address this in a later update.
+    /**
+     Typically, we avoid embedding API keys directly into code for security reasons. However, for the sake of convenience during the review process, and as it is not the primary focus of the current assignment, the API key is hardcoded in this instance.
+     */
     static let apikey = "BqxcH1X0nbDP0LnkCVoOHcl1ENyncboD"
     static let owner = "0x85fD692D2a075908079261F5E351e7fE0267dB02"
 }
